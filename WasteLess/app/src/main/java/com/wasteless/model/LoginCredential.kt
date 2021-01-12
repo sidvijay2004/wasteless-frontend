b7508aaa264da2133ca2db3339bbccbd016d11b8
Siddharth Vijayasankar <sidvijay@sidvijay.local>
2021-01-12 18:45:34 -0500

    added copyright statement to every file

--- WasteLess/app/src/main/java/com/wasteless/model/LoginCredential.kt
+++ WasteLess/app/src/main/java/com/wasteless/model/LoginCredential.kt
@@ -1,3 +1,13 @@
+/* Copyright (C) Wasteless - All Rights Reserved
+
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+
+ * Proprietary and confidential
+
+ * Written by Siddharth Vijayasankar <sidvijay2004@gmail.com>, January 2021
+
+ */
+
 package com.wasteless.model
 
 data class LoginCredential(
